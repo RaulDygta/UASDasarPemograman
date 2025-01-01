fdf1fde0f57aa2af14dd36ff0610e697121c9884
DELL <DELL@DESKTOP-VBRI3F1>
2025-01-01 19:30:52 +0700

    first commit

---
+++ src/HaloProject/HaloProjectNew.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package HaloProject;
+
+public class HaloProjectNew {
+    public static void main(String[] args) {
+        System.out.println("Halo Semarang");
+    }
+}
