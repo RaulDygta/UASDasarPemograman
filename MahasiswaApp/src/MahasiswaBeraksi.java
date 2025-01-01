37983e5f4015793b38afc40b1203d832a3af3038
RaulDygta <rauldygta@gmail.com>
2025-01-01 23:38:53 +0700

    Add files via upload

---
+++ MahasiswaApp/src/MahasiswaBeraksi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DELL
+ */
+public class MahasiswaBeraksi {
+     public static void main(String[] args) {
+        // Membuat objek dari class Mahasiswa
+        Mahasiswa mhs = new Mahasiswa();
+
+        // Memanggil method-method dari class Mahasiswa
+        mhs.membaca();
+        mhs.nyontek();
+        mhs.modifikasi();
+    }
+}
